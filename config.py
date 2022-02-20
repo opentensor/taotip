ec2efc034fbcf4665bdd5f95164e279fed5178fd
Cameron Fairchild <cameron@fairchild.dev>
2022-02-20 04:22:34 +0000

    updated deposit/withdraw validate

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ PROMPT = '!tip'
 COLDKEY_SECRET=b'<secret>'
 MONGO_URI="mongodb+srv://<>"
 BAL_PROMPT="(!balance)|(!bal)"
-DEP_PROMPT="!deposit"
-WIT_PROMPT="!withdraw"
+DEP_PROMPT="!deposit ([1-9][0-9]*|)((0|)\.[0-9]+)?"
+WIT_PROMPT="!withdraw (5([A-z]|[0-9])+) ([1-9][0-9]*|)((0|)\.[0-9]+)?"
 HELP_PROMPT="(!help)|(!h)"
 MAINTAINER="@<>#<>"
 DEP_ACTIVE_TIME=17200.0 # seconds
@@ -15,5 +15,5 @@ TESTING=False
 NUM_DEPOSIT_ADDRESSES=10
 HELP_STR="To get your balance, type: `!balance` or `!bal`\n" + \
         "To deposit tao, type: `!deposit <amount>`\n" + \
-        "To withdraw your tao, type: `!withdraw <amount>`\n" + \
+        "To withdraw your tao, type: `!withdraw <address> <amount>`\n" + \
         f"For help, type: `!h` or `!help` or contact {MAINTAINER}\n"
