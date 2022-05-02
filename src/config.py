f6ce8654cd941c99a84fa7ea560d811b88142c0e
camfairchild <cameron@fairchild.dev>
2022-05-02 18:11:08 -0400

    change naming for mongodb

--- src/config.py
+++ src/config.py
@@ -1,10 +1,8 @@
 DISCORD_TOKEN = ''
 CURRENCY = r'tao|t|tau|Tao|Tau|𝜏'
 PROMPT = '!tip'
-BOT_ID = ''
-COLDKEY_SECRET=b''
-MONGO_URI="mongodb://taotip:prod_pass@mongodb_container:27017/prod?retryWrites=true&w=majority"
-MONGO_URI_TEST="mongodb://taotip:taotip@mongodb_container:27017/test?retryWrites=true&w=majority"
+MONGO_URI="mongodb://taotip:prod_pass@mongodb:27017/prod?retryWrites=true&w=majority"
+MONGO_URI_TEST="mongodb://taotip:taotip@mongodb:27017/test?retryWrites=true&w=majority"
 BAL_PROMPT="(!balance)|(!bal)"
 DEP_PROMPT=f"!deposit ([1-9][0-9]*|0)(\.|\.[0-9]+)?\s*({CURRENCY}|)?"
 WIT_PROMPT=f"!withdraw (5([A-z]|[0-9])+)\s+([1-9][0-9]*|0)(\.|\.[0-9]+)?\s*({CURRENCY}|)?"
