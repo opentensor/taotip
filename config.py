afd80f37234b88fecd9d74b3d95937a1b65757f8
camfairchild <cameron@fairchild.dev>
2022-05-02 15:49:30 -0400

    updated config

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
-DISCORD_TOKEN = '<>'
+DISCORD_TOKEN = ''
+CURRENCY = r'tao|t|tau|Tao|Tau|𝜏'
 PROMPT = '!tip'
-COLDKEY_SECRET=b'<secret>'
-MONGO_URI="mongodb+srv://<>"
+BOT_ID = ''
+COLDKEY_SECRET=b''
+MONGO_URI="mongodb+srv://taotip:password@mongodb_container/test?retryWrites=true&w=majority"
 BAL_PROMPT="(!balance)|(!bal)"
-DEP_PROMPT="!deposit ([1-9][0-9]*|)((0|)\.[0-9]+)?"
-WIT_PROMPT="!withdraw (5([A-z]|[0-9])+) ([1-9][0-9]*|)((0|)\.[0-9]+)?"
+DEP_PROMPT=f"!deposit ([1-9][0-9]*|0)(\.|\.[0-9]+)?\s*({CURRENCY}|)?"
+WIT_PROMPT=f"!withdraw (5([A-z]|[0-9])+)\s+([1-9][0-9]*|0)(\.|\.[0-9]+)?\s*({CURRENCY}|)?"
 HELP_PROMPT="(!help)|(!h)"
-MAINTAINER="@<>#<>"
-DEP_ACTIVE_TIME=17200.0 # seconds
-DEPOSIT_INTERVAL=60.0 # seconds
-CHECK_ALL_INTERVAL=600.0 # seconds
+MAINTAINER="@#"
+DEP_ACTIVE_TIME=600.0 # seconds
+DEPOSIT_INTERVAL=24.0 # seconds
+CHECK_ALL_INTERVAL=300.0 # seconds
 SUBTENSOR_ENDPOINT="<>:9944"
-TESTING=False
+TESTING=True
 NUM_DEPOSIT_ADDRESSES=10
 HELP_STR="To get your balance, type: `!balance` or `!bal`\n" + \
         "To deposit tao, type: `!deposit <amount>`\n" + \
