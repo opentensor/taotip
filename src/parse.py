7f08d37120fc43177669d2751e7fc9b088984baf
camfairchild <cameron@fairchild.dev>
2022-05-03 12:51:53 -0400

    parsing passes tests

--- src/parse.py
+++ src/parse.py
@@ -1,13 +1,23 @@
 import re
 from . import config
-amount_check = re.compile(r' (([1-9][0-9]*|0)(\.*[0-9]*))\s*(' + config.CURRENCY + r'|$)')
+from substrateinterface.utils.ss58 import is_valid_ss58_address
+amount_check = re.compile(r'> (([1-9][0-9]*|0)(\.[0-9]*)?)\s*(' + config.CURRENCY + r'|)$')
 
 def get_amount(message: str) -> float:
     """
     Returns the amount of tao in the message
     """
-    amount_str = amount_check.search(message).groups()[0]
+    try:
+        groups = amount_check.search(message).groups()
+        amount_str = groups[0]
+    except AttributeError as e:
+        raise ValueError('Invalid amount')
     return float(amount_str)
 
 def get_coldkeyadd(message: str) -> str:
-    return message.split()[1]
+    address: str = message.split()[1]
+
+    if not is_valid_ss58_address(address, 42):
+        raise ValueError('Invalid address')
+
+    return address
