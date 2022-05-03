0fbb69452821a4358f6c67b4a48141087d000271
camfairchild <cameron@fairchild.dev>
2022-05-03 19:04:02 -0400

    fix tests for async

--- test/test_db.py
+++ test/test_db.py
@@ -1,11 +1,15 @@
-from binascii import hexlify
-from random import random
+import imp
+import random
 import bittensor
 import mongomock
 import unittest
-from ..src import api, db, config
+import asyncio
+import pytest
+from cryptography.fernet import Fernet
+from ..src import api, db
+from ..src.db import Address
 
-class DBTestCase(unittest.TestCase):
+class DBTestCase(unittest.IsolatedAsyncioTestCase):
     @classmethod
     def setUpClass(cls):
         cls._api = api.API(testing=True)
@@ -17,6 +21,7 @@ class DBTestCase(unittest.TestCase):
         self._db.db.accounts.drop()
         self._db.db.balances.drop()
         self._db.db.tips.drop()
+        
 class TestAddressLock(DBTestCase):
     async def test_address_lock_already_locked(self):
         # Create locked address in db
@@ -27,7 +32,7 @@ class TestAddressLock(DBTestCase):
 
         # Check if can acquire lock
         self.assertFalse(await self._db.lock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
-        
+
     async def test_address_lock_not_locked(self):
         # Create unlocked address in db   
         self._db.db.addresses.insert_one({
@@ -46,10 +51,10 @@ class TestAddressLock(DBTestCase):
         })
 
         # Should acquire lock
-        self.assertTrue(await self._db.unlock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
+        self.assertTrue(await self._db.lock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
 
         # Should not acquire lock
-        self.assertFalse(await self._db.unlock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
+        self.assertFalse(await self._db.lock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
 
     async def test_address_lock_locked_with_expiry(self):
         # Create locked address in db
@@ -59,49 +64,49 @@ class TestAddressLock(DBTestCase):
         })
 
         # Set lock expiry
-        ## 5 seconds
-        await self._db.set_lock_expiry('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx', 5)
+        ## 3 seconds
+        await self._db.set_lock_expiry('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx', 3)
     
-        # Should acquire lock. Lock expires in 5 seconds
-        self.assertFalse(await self._db.unlock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
+        # Should acquire lock. Lock expires in 3 seconds
+        self.assertFalse(await self._db.lock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
 
-        # Wait for lock to expire, 6 seconds
-        await self._api.sleep(6)
+        # Wait for lock to expire, 4 seconds
+        await asyncio.sleep(4)
+
+        unlocked: int = await self._db.remove_old_locks()
+        self.assertEqual(unlocked, 1)
 
         # Should acquire lock, lock has expired
-        self.assertTrue(await self._db.unlock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
-
+        self.assertTrue(await self._db.lock_addr('5DkpHw4sL5ZD9FQ8Z5Z5xX3z8bwZKXzQXx'))
 
 class TestAddressEncrypt(DBTestCase):
     async def test_encrypt(self):
-        key = '0x' + '0' * 64
-        key_bytes = bytes.fromhex(key)
-        addr: db.Address = self._api.create_address(key_bytes)
+        key_bytes = Fernet.generate_key()
+        addr: Address = self._api.create_address(key_bytes)
         mnemonic_to_bytes: bytes = bytes(addr.mnemonic, 'utf-8')
         enc_mnemonic: bytes = addr.get_encrypted_mnemonic()
         self.assertNotEqual(mnemonic_to_bytes.hex(), enc_mnemonic.hex())
 
     async def test_decrypt(self):
-        key = '0x' + '0' * 64
-        key_bytes = bytes.fromhex(key)
-        addr: db.Address = self._api.create_address(key_bytes)
+        key_bytes = Fernet.generate_key()
+        addr: Address = self._api.create_address(key_bytes)
         enc_mnemonic: bytes = addr.get_encrypted_mnemonic()
-        dec_mnemonic: str = addr.__unencrypt(enc_mnemonic, key_bytes)
-        self.assertEqual(dec_mnemonic, addr.mnemonic)
+
+        dec_addr: Address = Address(addr.address, enc_mnemonic, key_bytes, decrypt=True)
+        self.assertEqual(dec_addr.mnemonic, addr.mnemonic)
 
     async def test_decrypt_wrong_key(self):
-        key = '0x' + '0' * 64
-        key_bytes = bytes.fromhex(key)
-        addr: db.Address = self._api.create_address(key_bytes)
+        key_bytes = Fernet.generate_key()
+        addr: Address = self._api.create_address(key_bytes)
         enc_mnemonic: bytes = addr.get_encrypted_mnemonic()
-        key_bytes = bytes.fromhex('0x' + '1' * 64)
-        dec_mnemonic: str = addr.__unencrypt(enc_mnemonic, key_bytes)
-        self.assertNotEqual(dec_mnemonic, addr.mnemonic)
+        
+        wrong_key_bytes = Fernet.generate_key()
+        # Raises error if wrong key
+        self.assertRaises(Exception, Address, addr.address, enc_mnemonic, wrong_key_bytes, decrypt=True)
 
     async def test_get_encrypted_mnemonic_from_db(self):
-        key = '0x' + '0' * 64
-        key_bytes = bytes.fromhex(key)
-        addr: db.Address = self._api.create_address(key_bytes)
+        key_bytes = Fernet.generate_key()
+        addr: 'db.Address' = self._api.create_address(key_bytes)
         # Put address in db
         self._db.db.addresses.insert_one({
             'address': addr.address,
@@ -110,13 +115,14 @@ class TestAddressEncrypt(DBTestCase):
         })
 
         # Get address from db, should be decrypted
-        addr_from_db = await self._db.get_address(addr.address, key=key_bytes)
-        self.assertEqual(addr_from_db.mnemonic, addr.mnemonic)
+        addr_from_db = self._db.get_address(addr.address, key=key_bytes)
+        keypair = bittensor.Keypair.create_from_mnemonic(addr_from_db.mnemonic)
+        self.assertEqual(addr.address, keypair.ss58_address)
+
 
     async def test_get_encrypted_mnemonic_from_db_wrong_key(self):
-        key = '0x' + '0' * 64
-        key_bytes = bytes.fromhex(key)
-        addr: db.Address = self._api.create_address(key_bytes)
+        key_bytes = Fernet.generate_key()
+        addr: 'db.Address' = self._api.create_address(key_bytes)
         # Put address in db
         self._db.db.addresses.insert_one({
             'address': addr.address,
@@ -124,14 +130,14 @@ class TestAddressEncrypt(DBTestCase):
             'locked': False
         })
 
+        wrong_key_bytes = Fernet.generate_key() 
         # Get address from db, should be decrypted
-        addr_from_db = await self._db.get_address(addr.address, key=bytes.fromhex('0x' + '1' * 64))
-        self.assertNotEqual(addr_from_db.mnemonic, addr.mnemonic)
+        addr_from_db = self._db.get_address(addr.address, key=wrong_key_bytes)
+        self.assertIsNone(addr_from_db) # Should be None because wrong key
 
     async def test_create_address_get_encrypted_mnemonic_from_db(self):
-        key = '0x' + '0' * 64
-        key_bytes = bytes.fromhex(key)
-        addr: str = await self._db.create_new_addr()
+        key_bytes = Fernet.generate_key()
+        addr: str = await self._db.create_new_addr(key_bytes)
 
         # Get address from db, should be encrypted
         encrypted_addr = self._db.db.addresses.find_one({'address': addr})
@@ -142,17 +148,16 @@ class TestAddressEncrypt(DBTestCase):
         encrypted_mnemonic = encrypted_addr['mnemonic']
         
         # Get address from db, should be decrypted
-        addr_from_db = await self._db.get_address(addr.address, key=key_bytes)
+        addr_from_db = self._db.get_address(addr, key=key_bytes)
         self.assertNotEqual(bytes(addr_from_db.mnemonic, 'utf-8'), encrypted_mnemonic)
 
         # Decrypt mnemonic
-        dec_mnemonic = addr_from_db.__unencrypt(encrypted_mnemonic, key_bytes)
-        self.assertEqual(dec_mnemonic, addr_from_db.mnemonic)
+        dec_addr: Address = Address(addr.address, encrypted_mnemonic, key_bytes, decrypt=True)
+        self.assertEqual(dec_addr.mnemonic, addr_from_db.mnemonic)
 
     async def test_decrypt_on_db(self):
-        key = '0x' + '0' * 64
-        key_bytes = bytes.fromhex(key)
-        addr: db.Address = self._api.create_address(key_bytes)
+        key_bytes = Fernet.generate_key()
+        addr: 'db.Address' = self._api.create_address(key_bytes)
         self._db.db.addresses.insert_one({
             'address': addr.address,
             'mnemonic': addr.mnemonic,
@@ -166,26 +171,26 @@ class TestBalanceChange(DBTestCase):
         user_id = random.randint(0, 1000000)
         bal = random.randint(0, 1000000)
         self._db.db.balances.insert_one({
-            'discord_use': user_id,
+            'discord_user': user_id,
             'balance': bal
         })
 
         # Check balance
-        self.assertEqual(await self._db.check_balance(user_id), bal)
+        self.assertEqual(await self._db.check_balance(user_id), bittensor.Balance.from_rao(bal).tao)
     
     async def test_update_balance(self):
         # Create user with balance
         user_id = random.randint(0, 1000000)
         bal = random.randint(0, 1000000)
         self._db.db.balances.insert_one({
-            'discord_use': user_id,
+            'discord_user': user_id,
             'balance': bal
         })
 
         # Check balance
-        self.assertEqual(await self._db.check_balance(user_id), bal)
+        self.assertEqual(await self._db.check_balance(user_id), bittensor.Balance.from_rao(bal).tao)
         # Update balance
         update_amount = random.random() * 1000 # Random float between 0 and 1000
         bal_new: bittensor.Balance = bittensor.Balance.from_rao(bal) + bittensor.Balance.from_float(update_amount)
         await self._db.update_balance(user_id, update_amount)
-        self.assertEqual(await self._db.check_balance(user_id), bal_new.to_rao())
+        self.assertEqual(await self._db.check_balance(user_id), bal_new.tao)
