5051d84e63f105ee826cc2bc0950d71625b4194d
camfairchild <cameron@fairchild.dev>
2022-05-04 15:18:23 -0400

    add some tests to check the chain functions in api

---
+++ test/test_chain.py
@@ -0,0 +1,31 @@
+import random
+from types import SimpleNamespace
+from typing import Dict
+from unittest.mock import MagicMock
+
+import bittensor
+from cryptography.fernet import Fernet
+
+from ..src import api, db
+from .test_db import DBTestCase
+
+"""
+Testing all the chain functions in the api.py file.
+Mocks the blockchain and the database.
+"""
+class TestSendTransaction(DBTestCase):
+    _api: api.API
+    _db: db.Database
+    
+    def test_send_transaction(self):
+        pass
+
+    def test_check_balance(self):
+        pass
+
+    def test_check_balance_with_invalid_address(self):
+        pass
+
+    def test_check_balance_after_transaction(self):
+        pass
+    
