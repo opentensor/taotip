32ac57ac08399035950613adbe4682e98fb3b981
camfairchild <cameron@fairchild.dev>
2022-05-12 11:14:34 -0400

    add mui

--- taotip-export/client/src/App.js
+++ taotip-export/client/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> Login with discord to export your mnemonic. </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/auth/discord"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click to Login
         </a>
       </header>
     </div>
