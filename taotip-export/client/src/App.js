8056fc0101fe120e17231b5ca4d54463557fc35d
camfairchild <cameron@fairchild.dev>
2022-05-12 17:13:23 -0400

    first page is good

--- taotip-export/client/src/App.js
+++ taotip-export/client/src/App.js
@@ -1,20 +1,22 @@
-import logo from "./logo.svg";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import DiscordIcon from './DiscordIcon';
+import logo from "./logo.png";
 import "./App.css";
+import { ThemeProvider } from '@mui/material/styles';
+import { theme as DiscordTheme } from './DiscordTheme';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Login with discord to export your mnemonic. </p>
-        <a
-          className="App-link"
-          href="/auth/discord"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click to Login
-        </a>
+        <p> Authenticate below to export your mnemonic. </p>
+        <ThemeProvider theme={DiscordTheme}>
+          <Button href="/auth/discord" variant="contained" color="blurple" sx={{ height: 1/2 }} startIcon={<DiscordIcon />}>
+            Authenticate with Discord
+          </Button>
+        </ThemeProvider>
       </header>
     </div>
   );
---
+++ taotip-export/client/src/DiscordTheme.jsx
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    blurple: {
+      main: '#5865F2',
+    },
+  },
+});
