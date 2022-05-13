435029d6e5920d9854422b5daf9825cb5a1dc414
camfairchild <cameron@fairchild.dev>
2022-05-12 20:34:13 -0400

    added error catching

--- taotip-export/client/src/Export.jsx
+++ taotip-export/client/src/Export.jsx
@@ -10,27 +10,46 @@ import "./Export.css";
 
 function Export() {
   const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState(false);
+  const [errorText, setErrorText] = React.useState("");
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
   const handleExport = async () => {
-    const response = await axios.post(
-        "/api/export",
-        { password });
-        // Download the file from json
-        if (response.status === 200) {
-            const address = response.data.address;
-            const file = new Blob([JSON.stringify(response.data)], { type: "application/json" });
-            const fileURL = URL.createObjectURL(file);
-            const link = document.createElement("a");
-            link.href = fileURL;
-            link.setAttribute("download", `${address}-taotip-export.json`);
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);       
-        } 
+        setError(false);
+        try {
+            const response = await axios.post(
+                "/api/export",
+                {
+                    password: password,
+                }
+            );
+            // Download the file from json
+            if (response.status === 200) {
+                const address = response.data.address;
+                const file = new Blob([JSON.stringify(response.data)], { type: "application/json" });
+                const fileURL = URL.createObjectURL(file);
+                const link = document.createElement("a");
+                link.href = fileURL;
+                link.setAttribute("download", `${address}-taotip-export.json`);
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);       
+            } else if (response.status === 404) {
+                // User not found in the database
+                setError(true);
+                setErrorText("User not found");
+            } else {
+                // error
+                throw new Error(`status: ${response.status}; ${response.data.error}`);
+            }
+        } catch (error) {
+            console.log("Error", error);
+            setError(true);
+            setErrorText("Unknown Error");
+        }
     };
 
   return (
@@ -39,19 +58,18 @@ function Export() {
         <p> You're authenticated! Download your wallet export below. </p>
         <div className="Export-form">
           <FormControl>
-            <InputLabel htmlFor="password">Export Password</InputLabel>
             <TextField
               id="password"
               type="password"
+              label="Export Password"
               required
-              aria-describedby="password-text"
               variant="filled"
               value={password}
               onChange={handlePasswordChange}
+              error={error}
+              helperText={error ? errorText :
+                  "This password will be used to encrypt your wallet for safe export."}
             />
-            <FormHelperText id="password-text" margin="normal">
-              This password is used to encrypt your wallet for safe export.
-            </FormHelperText>
             <Button
               href="#"
               variant="contained"
