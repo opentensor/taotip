991342b5c4dc2b085755320e40cd6f87241d7e70
Cameron Fairchild <cameron@opentensor.ai>
2022-08-31 19:07:47 -0400

    .

--- taotip/main.py
+++ taotip/main.py
@@ -109,9 +109,7 @@ def main() -> None:
                 await ctx.send("Invalid recipient", ephemeral=True)
                 return interactions.StopCommand()
 
-            await ctx.defer(ephemeral=True)
             await event_handlers.tip_user(config, _db, ctx, ctx.user, recipient.user, Balance.from_tao(amount))
-            await ctx.send(None)
 
         @bot.command(
             name="tip",
