261abdaf6a6a4ff8f01b5dbfac253f1182abc5e0
Cameron Fairchild <cameron@opentensor.ai>
2022-08-31 02:01:52 -0400

    use on start instead

--- taotip/main.py
+++ taotip/main.py
@@ -12,15 +12,15 @@ def main() -> None:
     bot = interactions.Client(token=config.DISCORD_TOKEN)
 
     @bot.event
-    async def on_ready():
+    async def on_start():
         _db: Database = None
         _api: api.API = None
         _api, _db = await event_handlers.on_ready_(bot, config)
 
         if _api is None or _db is None:
             print("Failed to initialize\nRetrying in 5s...")
-            await asyncio.sleep(5) # wait 5 seconds
-            await on_ready()
+            await asyncio.sleep(5)  # wait 5 seconds
+            await on_start()
             return
 
         # add all commands
@@ -28,13 +28,16 @@ def main() -> None:
         # add to client loop
         bot._loop.create_task(welcome_new_users(_db, bot, config))
 
-    async def welcome_new_users(_db: Database, client: interactions.Client, config: Config):
+    async def welcome_new_users(
+        _db: Database, client: interactions.Client, config: Config
+    ):
         while True:
             await event_handlers.welcome_new_users(_db, client, config)
             # sleep until next check
             await asyncio.sleep(config.NEW_USER_CHECK_INTERVAL)
 
     bot.start()
-    
+
+
 if __name__ == "__main__":
     main()
