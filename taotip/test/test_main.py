e964b576abc8ff3215961d5471be40e612a0f6f8
Cameron Fairchild <cameron@opentensor.ai>
2022-08-31 00:26:03 -0400

    switch to interactions and update tests

--- taotip/test/test_main.py
+++ taotip/test/test_main.py
@@ -1,20 +1,17 @@
-import asyncio
 import copy
 import random
 import unittest
-from datetime import datetime, timedelta
 from types import SimpleNamespace
-from typing import Coroutine
 from unittest.mock import AsyncMock, MagicMock, call, patch
 
 import bittensor
-import discord
+import interactions
 from callee import Contains
 from cryptography.fernet import Fernet
 
-from ..src import config, db
-from ..src import event_handlers as main
-from .test_db import DBTestCase
+from taotip.src import config, db
+from taotip.src import event_handlers as main
+from taotip.test.test_db import DBTestCase
 
 mock_config_: SimpleNamespace = SimpleNamespace(
     DISCORD_TOKEN = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-', k=59)),
@@ -24,10 +21,6 @@ mock_config_: SimpleNamespace = SimpleNamespace(
     COLDKEY_SECRET=Fernet.generate_key(),
     MONGO_URI="mongodb://taotip:pass_prod@mongodb:27017/prod?retryWrites=true&w=majority",
     MONGO_URI_TEST="mongodb://taotip:pass_test@mongodb:27017/test?retryWrites=true&w=majority",
-    BAL_PROMPT="!balance|!bal",
-    DEP_PROMPT=f"!deposit",
-    WIT_PROMPT=f"!withdraw (5([A-z]|[0-9])+)\s+([1-9][0-9]*|0)(\.|\.[0-9]+)?\s*(<currency>|)?",
-    HELP_PROMPT="!help|!h",
     MAINTAINER="@#",
     DEP_ACTIVE_TIME=600.0, # seconds
     DEPOSIT_INTERVAL=24.0, # seconds
@@ -41,7 +34,6 @@ mock_config_: SimpleNamespace = SimpleNamespace(
             f"For help, type: `!h` or `!help` or contact <maintainer>\n"
 )
 mock_config_.HELP_STR = mock_config_.HELP_STR.replace('<maintainer>', mock_config_.MAINTAINER)
-mock_config_.WIT_PROMPT = mock_config_.WIT_PROMPT.replace('<currency>', mock_config_.CURRENCY)
 
 
 class TestMain(DBTestCase):
@@ -50,126 +42,73 @@ class TestMain(DBTestCase):
     def setUp(self):
         self.mock_config = config.Config(mock_config_)
 
-    async def test_bal(self):
+    async def test_do_balance_check(self):
         bal: bittensor.Balance = bittensor.Balance.from_rao(random.randint(1, 10000000))
         user: int = random.randint(1, 10000000)
-        bot_id: int = user + 1 # not the same as user
+        bot_id: str = str(user + 1) # not the same as user
+
         # Insert user balance into db
         self._db.db.balances.insert_one({
             'discord_user': user,
             'balance': bal.rao
         })   
 
-        input_str_bal = '!bal'
-
-        mock_send = AsyncMock(
-            return_value=None
+        mock_user = MagicMock(
+            spec=interactions.User,
+            id=user,
+            bot=False,  
         )
-        mock_message = SimpleNamespace(
-            content=input_str_bal,
+
+        mock_ctx = MagicMock(
+            spec=interactions.CommandContext,
             author=SimpleNamespace(
-                id=user
+                id=user,
+                bot=False,
+                user=mock_user,
             ),
             channel=MagicMock(
-                spec=discord.channel.DMChannel,
-                send=mock_send
-            )
-        )
-        mock_client = MagicMock(
-            spec=discord.Client,
-            fetch_user=MagicMock(
-                return_value=SimpleNamespace(
-                    id=user
-                ),
-                spec=discord.Member,
+                spec=interactions.Channel,
+                type=interactions.ChannelType.DM, # checking balance in DM
             ),
-            user=SimpleNamespace(
-                id=bot_id
-            )
+            send=AsyncMock(
+                return_value=None
+            ),
+            application_id=bot_id,
         )
 
-        mock_config = copy.deepcopy(self.mock_config)
-        mock_config.BAL_PROMPT = '!bal|!balance'
         with patch.object(self._db, 'check_balance', return_value=bal) as mock_check_balance:
-            await main.on_message_(self._db, mock_client, mock_message, mock_config)
+            await main.do_balance_check(self.mock_config, self._db, mock_ctx, mock_user)
             mock_check_balance.assert_called_once_with(user)
 
-
-    async def test_balance(self):
-        bal: bittensor.Balance = bittensor.Balance.from_rao(random.randint(1, 10000000))
-        user: int = random.randint(1, 10000000)
-        bot_id: int = user + 1 # not the same as user
-        # Insert user balance into db
-        self._db.db.balances.insert_one({
-            'discord_user': user,
-            'balance': bal.rao
-        })   
-
-        input_str_bal = '!balance'
-
-        mock_send = AsyncMock(
-            return_value=None
-        )
-        mock_message = SimpleNamespace(
-            content=input_str_bal,
-            author=SimpleNamespace(
-                id=user
-            ),
-            channel=MagicMock(
-                spec=discord.channel.DMChannel,
-                send=mock_send
-            )
-        )
-        mock_client = MagicMock(
-            spec=discord.Client,
-            fetch_user=MagicMock(
-                return_value=SimpleNamespace(
-                    id=user
-                ),
-                spec=discord.Member,
-            ),
-            user=SimpleNamespace(
-                id=bot_id
-            )
-        )
-
-        mock_config = copy.deepcopy(self.mock_config)
-        mock_config.BAL_PROMPT = '!bal|!balance'
-        with patch.object(self._db, 'check_balance', return_value=bal) as mock_check_balance:
-            await main.on_message_(self._db, mock_client, mock_message, mock_config)
-            mock_check_balance.assert_called_once_with(user)
-
-    async def test_deposit(self):
+    async def test_do_deposit(self):
         user_id: int = random.randint(1, 10000000)
         user: str = str(user_id)
         bot_id: str = str(user_id + 1) # not the same as user
 
-        input_str_dep = '!deposit'
 
         mock_send = AsyncMock(
             return_value=None
         )
-        mock_message = SimpleNamespace(
-            content=input_str_dep,
+
+        mock_user = MagicMock(
+            spec=interactions.User,
+            id=user,
+            bot=False,  
+        )
+
+        mock_ctx = MagicMock(
+            spec=interactions.CommandContext,
             author=SimpleNamespace(
-                id=user
+                id=user,
+                bot=False,
+                user=mock_user,
             ),
             channel=MagicMock(
-                spec=discord.channel.DMChannel,
-                send=mock_send
-            )
-        )
-        mock_client = MagicMock(
-            spec=discord.Client,
-            fetch_user=MagicMock(
-                return_value=SimpleNamespace(
-                    id=user
-                ),
-                spec=discord.Member,
+                spec=interactions.Channel,
+                type=interactions.ChannelType.DM, # checking balance in DM
             ),
-            user=SimpleNamespace(
-                id=bot_id
-            )
+            send=mock_send,
+            application_id=bot_id,
         )
 
         mock_addr_: db.Address = self._api.create_address(self.mock_config.COLDKEY_SECRET)
@@ -180,156 +119,106 @@ class TestMain(DBTestCase):
         mock_config.DEP_PROMPT = '!deposit'
 
         with patch.object(self._db, 'get_deposit_addr', return_value=mock_addr) as mock_get_dep_addr:
-            await main.on_message_(self._db, mock_client, mock_message, mock_config)
-
+            await main.do_deposit(self.mock_config, self._db, mock_ctx, mock_user)
+        
             mock_get_dep_addr.assert_called_once()
             # Sends message about fees, then sends message about deposit address
             mock_send.assert_has_calls([call(unittest.mock.ANY), call(Contains(dep_str_mock_1))])
 
-    async def test_withdraw(self):
+    async def test_do_withdraw(self):
         user_id: int = random.randint(1, 10000000)
         user: str = str(user_id)
         bot_id: str = str(user_id + 1) # not the same as user
 
         withd_addr = self._api.create_address(self.mock_config.COLDKEY_SECRET)
         amount: bittensor.Balance = bittensor.Balance.from_rao(random.randint(1, 10000000))
-        input_str_withd = f'!withdraw {withd_addr.address} {amount.tao} tao'
 
         mock_send = AsyncMock(
             return_value=None
         )
-        mock_message = SimpleNamespace(
-            content=input_str_withd,
+
+        mock_user = MagicMock(
+            spec=interactions.User,
+            id=user,
+            bot=False,  
+        )
+
+        mock_ctx = MagicMock(
+            spec=interactions.CommandContext,
             author=SimpleNamespace(
-                id=user
+                id=user,
+                bot=False,
+                user=mock_user,
             ),
             channel=MagicMock(
-                spec=discord.channel.DMChannel,
-                send=mock_send
-            )
-        )
-        mock_client = MagicMock(
-            spec=discord.Client,
-            fetch_user=MagicMock(
-                return_value=SimpleNamespace(
-                    id=user
-                ),
-                spec=discord.Member,
+                spec=interactions.Channel,
+                type=interactions.ChannelType.DM, # checking balance in DM
             ),
-            user=SimpleNamespace(
-                id=bot_id
-            )
+            send=mock_send,
+            application_id=bot_id,
         )
 
         mock_config = copy.deepcopy(self.mock_config)
         mock_new_balance = bittensor.Balance.from_rao(random.randint(1, 2) * amount.rao) - amount
 
         with patch.object(db.Transaction, 'withdraw', return_value=mock_new_balance.tao) as mock_withdraw:
-            await main.on_message_(self._db, mock_client, mock_message, mock_config)
+            await main.do_withdraw(mock_config, self._db, mock_ctx, mock_user, withd_addr.address, amount)
 
             mock_withdraw.assert_called_once_with(self._db, withd_addr.address, mock_config.COLDKEY_SECRET)
             mock_send.assert_awaited_once_with(Contains(f'Your new balance is: {mock_new_balance.tao} tao'))
 
-    async def test_tip(self):
+    async def test_tip_user(self):
         user: int = random.randint(1, 10000000)
         bot_id: int = user + 1 # not the same as user
         recipient: int = user + 2 # not the same as user or bot_id
         amount: bittensor.Balance = bittensor.Balance.from_rao(random.randint(1, 10000000))
-        input_str_tip = f'!tip <@!{recipient}> {amount.tao}'
 
         mock_send = AsyncMock(
             return_value=None
         )
-        mock_message = SimpleNamespace(
-            content=input_str_tip,
-            author=MagicMock(
+        mock_user_send = AsyncMock(
+            return_value=None
+        )
+
+        mock_user = MagicMock(
+            spec=interactions.User,
+            id=user,
+            bot=False,  
+            send=mock_user_send
+        )
+
+        mock_ctx = MagicMock(
+            spec=interactions.CommandContext,
+            author=SimpleNamespace(
                 id=user,
-                mention='<@!{}>'.format(user),
-                spec=discord.Member
+                bot=False,
+                user=mock_user,
+                send=mock_send,
             ),
             channel=MagicMock(
-                spec=discord.channel.TextChannel,
-                send=mock_send
+                spec=interactions.Channel,
+                type=interactions.ChannelType.DM, # checking balance in DM
             ),
-            mentions=[MagicMock(
-                spec=discord.Member,
-                bot=False,
-                id=recipient,
-                mention='<@!{}>'.format(recipient)
-            )]
+            application_id=bot_id,
         )
-        mock_client = MagicMock(
-            spec=discord.Client,
-            fetch_user=AsyncMock(
-                return_value=MagicMock(
-                    id=user,
-                    bot=False,
-                    mention='<@!{}>'.format(user),
-                    spec=discord.Member
-                ),
-            ),
-            user=SimpleNamespace(
-                id=bot_id,
-                bot=True,
-            )
+
+        mock_recipient = MagicMock(
+            spec=interactions.User,
+            id=recipient,
+            bot=False,
         )
 
         mock_tip_send = AsyncMock(
-            return_value=None
+            return_value=True # successful tip
         )
         mock_tip = SimpleNamespace(
             send=mock_tip_send
         )
 
-        mock_config = copy.deepcopy(self.mock_config)             
-        mock_config.PROMPT = '!tip'
         with patch.object(db.Tip, '__new__', return_value=mock_tip) as mock_tip_new:
-            await main.on_message_(self._db, mock_client, mock_message, mock_config)
+            await main.tip_user(self.mock_config, self._db, mock_ctx, mock_user, mock_recipient, amount)
             mock_tip_new.assert_called_once_with(db.Tip, user, recipient, amount)
             mock_tip_send.assert_called_once() # Someone tipped someone...
-
-    async def test_help(self):
-        user: int = random.randint(1, 10000000)
-        bot_id: int = user + 1 # not the same as user
-
-        input_str_help = '!help'
-
-        mock_send = AsyncMock(
-            return_value=None
-        )
-        mock_message = SimpleNamespace(
-            content=input_str_help,
-            author=SimpleNamespace(
-                id=user
-            ),
-            channel=MagicMock(
-                spec=discord.channel.DMChannel,
-                send=mock_send
-            )
-        )
-        mock_client = MagicMock(
-            spec=discord.Client,
-            fetch_user=MagicMock(
-                return_value=SimpleNamespace(
-                    id=user
-                ),
-                spec=discord.Member,
-            ),
-            user=SimpleNamespace(
-                id=bot_id
-            )
-        )
-
-        help_str_mock = "this is a fake help string"
-
-        mock_config = copy.deepcopy(self.mock_config)
-        mock_config.HELP_PROMPT = '!help'
-        mock_config.HELP_STR = help_str_mock
-
-        await main.on_message_(self._db, mock_client, mock_message, mock_config)
-        mock_send.assert_called_once_with(help_str_mock)           
-    
-    async def test_on_ready(self):
-        # TODO: test everything is ready after on_ready
-        self.assert_(False)
+            
+            mock_user_send.assert_not_called() # no error message
     
